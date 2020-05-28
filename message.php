2c1273262bb5368f3a492aaf31a32c5b8182605c
suman <dhakalsumn739@gmail.com>
2020-05-28 20:16:52 +0545

    message page is added

---
+++ message.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>stay home stay safe</title>
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&family=Lobster&family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/message.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="container main">
+        <div class="left-portion">
+            <div class="thankuText">
+                <h1 id="thanks">THANK YOU !!!</h1>
+                <h3>for your valuable time</h3>
+            </div>
+            <div class="messageOne">
+                <h1 id="quote">
+                "एकता ठुलो शक्ति <br/>
+                हो सधै <br/>
+                तर एक्लै हुनुमा नि <br/>
+                आज जित देखियो"<br/>
+             </h1>
+             </div>
+        </div>
+        <div class="right-portion">
+            <img src="images/SHSF.png" id="stayhomestaysafe">
+        </div>
+    </div>
+</div>
+<div class="button">
+    <button type="button" disabled>Chat-bot</button>
+</div>
+
+</body>
+</html>
