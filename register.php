a1046eb1a012f617840e24d7e69f78f31d8521d0
suman <dhakalsumn739@gmail.com>
2020-05-30 19:39:16 +0545

    error fixed

--- register.php
+++ register.php
@@ -2,6 +2,17 @@
 
 session_start();
 
+if (isset($_SESSION['registered'])){
+    header('Location:../covid-19 survey/message.php');
+}
+
+
+
+if($_SERVER["REQUEST_METHOD"] != "POST"){
+        header('Location:../covid-19 survey/personalDetails.php');
+ }
+
+
 
  if($_SERVER["REQUEST_METHOD"] != "POST")
         header('Location:../covid-19 survey/personalDetails.php');
@@ -14,32 +25,67 @@ $dbname ="covid-19Survey";
 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 
-if (!$conn)
+if (!$conn){
     die("error to connect:".mysqli_connect_error());
+}
 
 
 // upload user personal details
 
-echo "full name".$_SESSION["age"];  // why is this not showing
+$query = "INSERT INTO personalDetails(fullName,email,age,gender,address,isInfected) VALUES('".$_SESSION['fullName']."','".$_SESSION['email']."','".$_SESSION['age']."','".$_SESSION['gender']."','".$_SESSION['address']."','".$_SESSION['isInfected']."');";
+
+
+if(!mysqli_query($conn,$query)){
+    die("failed to register user information");
+}
+
+
+// uploading survay form data
 
 
 
-// $question1 = $_POST["question1"];
-
-$query = "insert into answers(Q_ID)";
 
 
 foreach($_POST as $key => $value ){
+
     if ($value != ""){
 
-        $Q_ID = get_string_between($key,'question','+'); //extracting question id
+        // $Q_ID = get_string_between($key,'question','+'); //extracting question id
+
+
+        $Q_ID =  (int) filter_var($key, FILTER_SANITIZE_NUMBER_INT);
+
         $answer = $value;
 
+        // getting user id
+        $query = "SELECT max(USER_ID) as userID from personalDetails";
 
+        $result = mysqli_query($conn,$query);
 
+        $result = mysqli_fetch_array($result);
 
+        $recent_UserID = $result['userID'];
+
+        echo $recent_UserID." ".$Q_ID." ".$answer;
+
+        $query = "INSERT INTO answers(USER_ID,Q_ID,answer) VALUES ('".$recent_UserID."','".$Q_ID."','".$answer."');";
+
+        if (!mysqli_query($conn,$query)){
+
+            $query = "DELETE from personalDetails order by USER_ID desc limit 1;";
+            mysqli_query($conn,$query);
+
+            die("failed to upload survey data");
+        }
+        else{
+            session_unset();
+            $_SESSION['registered'] = true;
+
+            header('Location:../covid-19 survey/message.php');
+        }
   }
 }
+
 
 
 
